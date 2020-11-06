4a40904c32f4ac53f795742a8870974274d8d99e
Chris Lee <chrislee910320@gmail.com>
2020-11-05 22:10:09 -0700

    create a GET/POST router and try to connect between backend and front-end

--- server/index.js
+++ server/index.js
@@ -3,13 +3,27 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/posts", postRoutes);
+
 const CONNECTION_URL =
   "mongodb+srv://chrislee910320:chrislee910320@cluster0.4s90c.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
 
-// https://www.mongodb.com/cloud/atlas
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
